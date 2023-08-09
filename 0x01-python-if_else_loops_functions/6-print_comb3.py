5c552007f5b26f5a4cb5036a60df67883f0fa184
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-08-09 14:18:41 -0700

    new

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -13,8 +13,8 @@ for i in range(0, 10):
     for n in range(i+1, 10):
         # 2nd digit
         if i == 8 and n == 9:
-            #last number is 89
+            # last number is 89
             print("{}{}".format(i, n))
         else:
-            #rest of numbers
+# rest of numbers
             print("{}{}".format(i, n), end=", ")
