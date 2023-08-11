d13aa7b181b660329e32402ae6331f869a646dd9
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-08-10 17:50:16 -0700

    new

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
+
+def magic_calculation(a, b, c):
+    # Turn byteCode to python
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
