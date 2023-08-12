5d53ece6773f71e681d334a4c109d122c83bf88d
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-08-11 18:46:23 -0700

    new

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-# imports the function def add(a, b): from the file add_0.py
-# and prints the result of the addition 1 + 2 = 3
-# You can only use the word add_0 once in your code
-# You are not allowed to use * for importing or __import__
-# Your code should not be executed when imported - by using __import__
 
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
