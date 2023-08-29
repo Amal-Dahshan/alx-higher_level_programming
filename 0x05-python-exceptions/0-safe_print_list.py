714148560019075c5b318cabdea15b131987ac0e
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-08-28 18:38:25 -0700

    new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@
 #  Returns: The number of elements printed.
 
 def safe_print_list(my_list=[], x=0):
-     count = 0
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
