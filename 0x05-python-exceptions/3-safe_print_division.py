30df2108a8c9b78e0fd3ad740274a87e75f3097b
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-08-28 19:14:50 -0700

    new

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     ''' function that divides 2 integers and prints the result.
     You can assume that a and b are integers
-    The result of the division should print on the finally: section preceded by Inside result:
+    The result of the division should print on the finally: section
     Returns the value of the division, otherwise: None
     You have to use try: / except: / finally:
     You have to use "{}".format() to print the result
@@ -14,4 +14,4 @@ def safe_print_division(a, b):
         div = None
     finally:
         print("Inside result: {}".format(div))
-    return (div) 
+    return (div)
