6a3a5db7ea0bd2296b8762645f784a5edcbcea4d
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-09-20 15:18:46 -0700

    new

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,12 +3,13 @@
 """Defines a base model class."""
 import json
 import csv
+import turtle
 
 
 class Base:
     """Base model.
 
-    This Represents the "base" for all other classes in project.
+    This Represents the "base" for all other classes in project 0x0C*.
 
     Private Class Attributes:
         __nb_object (int): Number of instantiated Bases.
@@ -144,3 +145,44 @@ class Base:
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw Rectangles and Squares using the turtle module.
+
+        Args:
+            list_rectangles (list): A list of Rectangle objects to draw.
+            list_squares (list): A list of Square objects to draw.
+        """
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
+
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,8 @@ from models.base import Base
 class Rectangle(Base):
     """Represent a rectangle."""
 
-def __init__(self, width, height, x=0, y=0, id=None):
-    """Initialize a new Rectangle.
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
 
         Args:
             width (int): The width of the new Rectangle.
@@ -21,11 +21,11 @@ def __init__(self, width, height, x=0, y=0, id=None):
             TypeError: If either of x or y is not an int.
             ValueError: If either of x or y < 0.
         """
-    self.width = width
-    self.height = height
-    self.x = x
-    self.y = y
-    super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
